b3b33fbed25810f9c11c45bb29e57a7fb34588d8
rawanfatima <rawanfatima@outlook.com>
2020-08-20 22:11:48 -0700

    test

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,5 @@
 // will contain all of the user-facing routes, such as the homepage and login page
-const sequelize = require('../config/connection');
+// const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 const router = require('express').Router();
 
